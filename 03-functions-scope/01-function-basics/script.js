4e024c66c4a43bd9cc381c343baaff82ad5146af
SarahPri <princesswangari09@gmail.com>
2024-08-18 14:45:08 +0300

    Creating a Function

---
+++ 03-functions-scope/01-function-basics/script.js
@@ -0,0 +1,21 @@
+//function that is just console log(displaying on the screen)
+
+function sum(num1, num2){
+    console.log(num1 + num2);
+
+}
+sum(3, 5);
+
+// returning a value
+function subtract(num1 , num2){
+    return num1 - num2;
+
+}
+const result = subtract(8, 4);
+console.log(result);
+
+//you can also console log this way
+function multiply(num1 , num2){
+    return num1 * num2;
+}
+const mult = console.log(multiply(3, 4));
