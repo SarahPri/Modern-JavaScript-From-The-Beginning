23870136d5f4418a0f842e538598ba9b15a6eede
SarahPri <princesswangari09@gmail.com>
2024-08-19 07:32:59 +0300

    function declaration and function expression

---
+++ 03-functions-scope/06-declaration-vs-expression/script.js
@@ -0,0 +1,10 @@
+function addKshSign(value){
+    return 'Ksh. ' + value;
+}
+console.log(addKshSign(100));
+
+const addPlusSign = function(value){
+    return '+' + value;
+};
+
+console.log(addPlusSign (254));
