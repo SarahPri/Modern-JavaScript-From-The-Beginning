46875f309ba34f9074f93e79565e7ba27b1bea16
SarahPri <princesswangari09@gmail.com>
2024-08-19 08:07:18 +0300

    Arrow Functions

--- 03-functions-scope/07-arrow-functions/script.js
+++ 03-functions-scope/07-arrow-functions/script.js
@@ -18,6 +18,12 @@ const subtract = (a,b) => a - b;
 console.log(subtract(90,40));
 
 
+//using callback function
+// that is passing a function inside a function
+const numbers1 = [1,2,3,4,5];
+numbers1.forEach(function(n){
+    console.log(n);
+})
 // Arrow function in a callback
 const numbers = [1,2,3,4,5];
 numbers.forEach (n => console.log(n));
