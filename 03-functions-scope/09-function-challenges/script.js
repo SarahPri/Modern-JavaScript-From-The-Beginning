f8905340647ade0b8e7e8e860967c69b4cdd5417
SarahPri <princesswangari09@gmail.com>
2024-08-21 06:37:39 +0300

    Function Challenge

---
+++ 03-functions-scope/09-function-challenges/script.js
@@ -0,0 +1,13 @@
+// function getCelsius(F){
+//     const celsius =  ((F-32)*5)/9;
+//     return Math.floor=celsius;
+
+
+// }
+// console.log(`The temperature is ${getCelsius(14)} \xB0C`);
+
+const getCelsius = fahrenheit => {
+    const celsius =  ((fahrenheit-32)*5)/9;
+    return Math.floor=celsius;
+}
+console.log(`The temperature is ${getCelsius(14)} \xB0C`);
