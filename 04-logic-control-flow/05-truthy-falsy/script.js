74b711fa6f10c3dfb6fedb514058cbf02527b781
SarahPri <princesswangari09@gmail.com>
2024-09-01 19:24:10 +0300

    Truthy & Falsy Values

--- 04-logic-control-flow/05-truthy-falsy/script.js
+++ 04-logic-control-flow/05-truthy-falsy/script.js
@@ -17,3 +17,21 @@ if(x){
 
 
 console.log(Boolean(x));
+
+// Truthy and Falsy caveats
+
+// const children = 3
+
+// if(children){
+//     console.log(`you have ${children} children`);
+// }else{
+//     console.log('Please enter number of children');
+// }
+
+const children = 0;
+// if(!isNaN(children))
+if(children !== undefined){
+    console.log(`you have ${children} children`);
+}else{
+    console.log('Please enter number of children');
+}
