3a1bd0d0c4387bfa2c550e61b96514d099dc4cf5
SarahPri <princesswangari09@gmail.com>
2024-09-01 19:31:52 +0300

    Truthy & Falsy Values

--- 04-logic-control-flow/05-truthy-falsy/script.js
+++ 04-logic-control-flow/05-truthy-falsy/script.js
@@ -35,3 +35,51 @@ if(children !== undefined){
 }else{
     console.log('Please enter number of children');
 }
+
+//checking for empty arrays
+
+// const post = ['Post One', 'Post Two'];
+
+// if (post.length > 0){
+//     console.log('List Posts');
+// }else{
+//     console.log('No Posts to list');
+// }
+
+
+const post = ['Post One', 'Post Two'];
+
+if (post.length > 0){
+    console.log('List Posts');
+}else{
+    console.log('No Posts to list');
+}
+
+//Checking for empty Objects
+// const user ={
+//     name: "Sarah"
+// }
+
+// if (user){
+//     console.log('List User');
+// }else{
+//     console.log('No User');
+// }
+
+
+// ================
+const user ={
+    
+}
+
+if (Object.keys(user).length > 0){
+    console.log('List User');
+}else{
+    console.log('No User');
+}
+
+// Loose Equality
+console.log(false == 0);
+console.log('' == 0);
+console.log(null == undefined);
+console.log(null === undefined);
