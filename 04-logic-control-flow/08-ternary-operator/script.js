5224dca68cdfae165fe5f544e375ed166e1504da
SarahPri <princesswangari09@gmail.com>
2024-09-01 20:05:15 +0300

    Ternary Operator

---
+++ 04-logic-control-flow/08-ternary-operator/script.js
@@ -0,0 +1,12 @@
+const age = 8;
+
+// using an if statement
+if(age >= 18){
+    console.log('You can vote');
+}else{
+    console.log("You Can't vote");
+}
+
+// Using Ternary Operator
+//syntax condition ? expressionIfTrue : expressionIfFalse;
+age >= 18 ? console.log('You can vote') : console.log('You can not vote');;
