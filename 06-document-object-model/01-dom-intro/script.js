245cdafa66d31039ec406c2531744738bf14edbc
SarahPri <princesswangari09@gmail.com>
2024-09-02 18:42:29 +0300

    DOM Intro

---
+++ 06-document-object-model/01-dom-intro/script.js
@@ -0,0 +1,11 @@
+// console.log(document.body.innerText);
+
+// console.log(document.links[0]);
+
+// Methods
+
+// document.write('Hello from Sarah')
+
+console.log(document.getElementById('main'));
+const main = document.getElementById('main');
+main.innerHTML = '<h1>Hello from main</h1>'
