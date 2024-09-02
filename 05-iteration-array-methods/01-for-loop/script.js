d2bf24db3db814d0f55e2a465cd12fe54df82f18
SarahPri <princesswangari09@gmail.com>
2024-09-02 10:45:32 +0300

    For Loop

--- 05-iteration-array-methods/01-for-loop/script.js
+++ 05-iteration-array-methods/01-for-loop/script.js
@@ -24,3 +24,16 @@ for (let i = 1; i<=10; i++){
         console.log(`${i} * ${j} = ${i * j}`);
     }
 }
+
+// Loop through an array
+const names = ['Sarah', 'Lucy', 'Sam', 'Jeff', 'Hilda'];
+
+for (let i = 0; i < names.length; i++){
+    
+    if ( i === 1){
+        console.log(names[i] + ' is the best');
+    }else {
+        console.log(names[i]);
+    }
+
+}
