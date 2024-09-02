49127057e965b6440fc8a4314f693ca4c6dfe45f
SarahPri <princesswangari09@gmail.com>
2024-09-02 12:26:46 +0300

    Filter

---
+++ 05-iteration-array-methods/08-filter/script.js
@@ -0,0 +1,38 @@
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+const evenNumbers = numbers.filter(function(item){
+    return item % 2 == 0;
+});
+console.log(evenNumbers);
+
+//Even Numbers
+// const oddNumbers = numbers.filter(function(item){
+//     return item % 3 == 0;
+// });
+// console.log(oddNumbers);
+
+// Short way of doing this
+
+const oddNumbers = numbers.filter((item) => item % 3 == 0);
+console.log(oddNumbers);
+
+
+// ===================================
+const companies = [
+    { name: 'Company One', category: 'Finance', start: 1981, end: 2004 },
+    { name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
+    { name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
+    { name: 'Company Four', category: 'Retail', start: 1989, end: 2010 },
+    { name: 'Company Five', category: 'Technology', start: 2009, end: 2014 },
+    { name: 'Company Six', category: 'Finance', start: 1987, end: 2010 },
+    { name: 'Company Seven', category: 'Auto', start: 1986, end: 1996 },
+    { name: 'Company Eight', category: 'Technology', start: 2011, end: 2016 },
+    { name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 },
+    ];
+
+// const longCompanies = companies.filter((item) => item.end >= item.start + 10);
+// console.log(longCompanies);
+
+const RetailCompanies = companies.filter((item) => item.category === "Retail")
+
+console.log(RetailCompanies);
