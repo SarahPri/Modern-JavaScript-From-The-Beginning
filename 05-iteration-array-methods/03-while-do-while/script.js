8a753933a55f410cb374b9540d2d93d4430f29bd
SarahPri <princesswangari09@gmail.com>
2024-09-02 11:11:17 +0300

    While & Do While Loop

---
+++ 05-iteration-array-methods/03-while-do-while/script.js
@@ -0,0 +1,35 @@
+// let i = 0;
+// while (i <=20 ){
+//     console.log('Number ' + i);
+//     i++;
+// }
+
+//Loop over arrays
+// const arr = [10,20,30,40,50];
+// while (i < arr.length){
+//     console.log(arr[i]);
+//     i++;
+// }
+
+// Nesting while Loop
+
+// let i = 0;
+// while (i <= 10){
+//     console.log('Number ' + i);
+
+//     let j = 0;
+//     while (j <=5){
+//         console.log(`${i} * ${j} = ${i * j}`);
+//         j++;
+//     }
+    
+//     i++;
+// }
+
+
+// Do while
+let i = 21;
+do{
+    console.log('Number ' + i);
+    i++;
+}while (i <=20);
