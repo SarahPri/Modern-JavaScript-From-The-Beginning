59e407b79b6436f417c018fa609a5e90bb591f62
SarahPri <princesswangari09@gmail.com>
2024-09-03 15:52:58 +0300

    Manipulating Styles & classes

---
+++ 06-document-object-model/14-styles-classes/script.js
@@ -0,0 +1,16 @@
+const text = document.querySelector("p");
+const itemList = document.querySelector(".item-list");
+const items = itemList.querySelectorAll("li");
+
+function run() {
+//   className
+// console.log(itemList.className);
+// text.className = 'card dark';
+
+//classList
+console.log(itemList.classList);
+
+itemList.classList.forEach(c => console.log(c))
+}
+
+document.querySelector('button').onclick = run;
