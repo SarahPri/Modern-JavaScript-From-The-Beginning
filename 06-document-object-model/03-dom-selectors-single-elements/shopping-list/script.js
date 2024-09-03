921035b37fff0d417e1e58106eb6ac2ddef662cd
SarahPri <princesswangari09@gmail.com>
2024-09-03 03:41:20 +0300

    dom selectors single elements\shopping list

---
+++ 06-document-object-model/03-dom-selectors-single-elements/shopping-list/script.js
@@ -0,0 +1,29 @@
+let output;
+output = document.head.children;
+output = document.body.children;
+
+output = document.doctype;
+output = document.domain;
+output = document.URL
+output = document.contentType;
+
+
+output = document.forms
+output = document.forms[0];
+output = document.forms[0].id;
+output = document.forms[0].method;
+output = document.forms[0].action;
+
+// document.forms[0].id = 'new-id';
+
+
+output = document.images;
+output = document.images[0];
+output = document.images[0].src;
+
+
+
+
+
+
+console.log(output);
