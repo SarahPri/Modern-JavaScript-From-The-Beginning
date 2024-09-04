32c9dd560543f73b0e4fa5ab2d5fedf7e1acc8fe
SarahPri <princesswangari09@gmail.com>
2024-09-04 14:16:39 +0300

    Callbacks

--- 09-asynchronous-javascript/03-callbacks/script.js
+++ 09-asynchronous-javascript/03-callbacks/script.js
@@ -1,6 +1,48 @@
-function toggle(e) {
-  console.log('callback ran');
-  e.target.classList.toggle('danger');
+// function toggle(e) {
+//   console.log('callback ran');
+//   e.target.classList.toggle('danger');
+// }
+
+// document.querySelector('button').addEventListener('click', toggle);
+
+
+const posts = [
+  {
+  title: 'Post one',
+  body: 'this is post one'
+},
+  {
+  title: 'Post two',
+  body: 'this is post two'
+},
+];
+
+function createPost(post, cb){
+  setTimeout(() => {
+    posts.push(post);
+    cb()
+
+  }, 2000);
 }
 
-document.querySelector('button').addEventListener('click', toggle);
+
+function getPosts(){
+  setTimeout(() => {
+    posts.forEach(function (post){
+      const div = document.createElement('div');
+      div.innerHTML = `<strong> ${post.title}</strong> - ${post.body}`;
+      document.querySelector('#posts').appendChild(div)
+
+    });
+
+    
+   }, 1000);
+}
+
+createPost({title: 'Post Three', body: 'This is Post Three'}, getPosts)
+
+
+
+
+
+// getPosts();
