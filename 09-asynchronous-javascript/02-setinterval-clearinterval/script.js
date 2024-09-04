19198867e1c9b9ea248ae3ef194068d412582036
SarahPri <princesswangari09@gmail.com>
2024-09-04 14:01:19 +0300

    setinterval & clear Interval

---
+++ 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
@@ -0,0 +1,47 @@
+// const intervalId = setInterval(myCallback, 2000);
+
+// function myCallback(){
+//     console.log(Date.now());
+
+// }
+
+let intervalId;
+
+function startChange() {
+  if (!intervalId) {
+    intervalId = setInterval(changeColor, 1000);
+  }
+}
+
+// function changeColor(){
+//     if(document.body.style.backgroundColor !== 'black'){
+//         document.body.style.backgroundColor = 'black'
+//         document.body.style.color = 'white'
+
+//     }else{
+//         document.body.style.backgroundColor = 'white'
+//         document.body.style.color = 'black'
+//     }
+// }
+
+// change a hexadecimal color
+
+function startChange() {
+    if (!intervalId) {
+      intervalId = setInterval(changeRandomColor, 1000);
+    }
+  }
+
+
+function changeRandomColor() {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+    document.body.style.backgroundColor = `#${randomColor}`
+}
+
+function stopChange() {
+  clearInterval(intervalId);
+}
+
+document.getElementById("start").addEventListener("click", startChange);
+document.getElementById("stop").addEventListener("click", stopChange);
