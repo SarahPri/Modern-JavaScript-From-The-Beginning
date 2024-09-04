6db4b6a5cac71c01849f02f073e8b0fcf6455303
SarahPri <princesswangari09@gmail.com>
2024-09-04 07:33:52 +0300

    mouse events

--- 07-events/02-mouse-events/shopping-list/script.js
+++ 07-events/02-mouse-events/shopping-list/script.js
@@ -1,1 +1,40 @@
 
+const logo = document.querySelector('img');
+
+const onClick = () => console.log('Click Event');
+const onDoubleClick = () => {
+    if (document.body.style.backgroundColor !== 'blue'){
+        document.body.style.backgroundColor = 'blue';
+        document.body.style.color = 'white';
+    }else{
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    }
+}
+
+const onRightClick = () => console.log('Right Clicked');
+
+const onMouseDown = () => console.log('On Mouse Down event');
+
+const onMouseUp = () => console.log('On Mouse Up');
+const onMouseWheel = () => console.log('On Mouse Wheel Event');
+const onMouseOver = () => console.log('On Mouse over  Event');
+const onMouseOut = () => console.log('On Mouse out Event');
+
+
+
+
+
+
+
+
+
+
+// Event Listeners
+logo.addEventListener('click', onClick)
+logo.addEventListener('dblclick', onDoubleClick)
+logo.addEventListener('contextmenu', onRightClick) //right click
+logo.addEventListener('mousedown', onMouseDown) //click mouse and hold
+logo.addEventListener('wheel', onMouseWheel) //release mouse
+logo.addEventListener('mouseover', onMouseOver) //release mouse
+logo.addEventListener('mouseout', onMouseOut) //release mouse
