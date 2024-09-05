c72f7d146fe77c8ecd9f295e4e97819da3e4ddcd
SarahPri <princesswangari09@gmail.com>
2024-09-05 16:30:11 +0300

    Error Handling With Async & Await

---
+++ 10-fetch-and-async-await/08-async-await-error-handling/script.js
@@ -0,0 +1,38 @@
+// const getUsers = async () => {
+  // try {
+    // const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+//     const response = await fetch('http://httpstat.us/500');
+
+//     if(!response.ok){
+//       throw new Error('Request Failed');
+//     }
+
+//     const data = await response.text();
+
+//     console.log(data);
+
+//   }catch (error){
+//     console.log(error);
+//   }
+  
+// };
+// getUsers();
+
+
+// ===================================
+// get posts
+const getPosts = async () => {
+    // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+const response = await fetch('http://httpstat.us/500');
+
+if(!response.ok){
+  throw new Error('Request Failed');
+}
+
+const data = await response.json();
+
+console.log(data);
+  };
+
+  getPosts().catch(error => console.log(error));
