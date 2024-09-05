8fd90c1d01fb28a6661172ef24a9ebc867baa767
SarahPri <princesswangari09@gmail.com>
2024-09-05 15:08:16 +0300

    Typicode Todos Mini-Project - Part 2

--- 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -1,48 +1,81 @@
-const apiUrl = "https://jsonplaceholder.typicode.com/todos";
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-const getToDos = () => {
-  fetch(apiUrl + "?_limit=5")
+const getTodos = () => {
+  fetch(apiUrl + '?_limit=10')
     .then((res) => res.json())
     .then((data) => {
-      data.forEach((todo) => addToDoToDOM(todo));
+      data.forEach((todo) => addTodoToDOM(todo));
     });
 };
 
-const addToDoToDOM = (todo) => {
-  const div = document.createElement("div");
+const addTodoToDOM = (todo) => {
+  const div = document.createElement('div');
+  div.classList.add('todo');
   div.appendChild(document.createTextNode(todo.title));
-  div.setAttribute("data-id", todo.id);
+  div.setAttribute('data-id', todo.id);
 
   if (todo.completed) {
-    div.classList.add("done");
+    div.classList.add('done');
   }
 
-  document.getElementById("todo-list").appendChild(div);
+  document.getElementById('todo-list').appendChild(div);
 };
 
-const createToDo = (e) => {
+const createTodo = (e) => {
   e.preventDefault();
 
   const newTodo = {
     title: e.target.firstElementChild.value,
-    completed: false
-  }
+    completed: false,
+  };
 
-  // Making a request and sending to the server
-  fetch (apiUrl, {
+  fetch(apiUrl, {
     method: 'POST',
     body: JSON.stringify(newTodo),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+    },
   })
-  .then(res => res.json())
-  .then(data => addToDoToDOM(data))
+    .then((res) => res.json())
+    .then((data) => addTodoToDOM(data));
 };
 
+const toggleCompleted = (e) => {
+  if (e.target.classList.contains('todo')) {
+    e.target.classList.toggle('done');
+
+    updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
+  }
+};
+
+const updateTodo = (id, completed) => {
+  fetch(`${apiUrl}/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ completed }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+const deleteTodo = (e) => {
+  if (e.target.classList.contains('todo')) {
+    const id = e.target.dataset.id;
+    fetch(`${apiUrl}/${id}`, {
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then(() => e.target.remove());
+  }
+};
 
 const init = () => {
-  document.addEventListener('DOMContentLoaded', getToDos );
-  document.querySelector('#todo-form').addEventListener('submit', createToDo )
+  document.addEventListener('DOMContentLoaded', getTodos);
+  document.querySelector('#todo-form').addEventListener('submit', createTodo);
+  document
+    .querySelector('#todo-list')
+    .addEventListener('click', toggleCompleted);
+  document.querySelector('#todo-list').addEventListener('dblclick', deleteTodo);
 };
+
 init();
